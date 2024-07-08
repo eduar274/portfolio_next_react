10391b87502114642ffd91d7c739094f76cfb317
eduar274 <eduar.jaimes@uniminuto.edu.co>
2024-07-08 18:59:05 -0500

    Initial commit

---
+++ src/pages/profile.js
@@ -0,0 +1,103 @@
+
+export const skills = [
+  {
+    skill: "JavaScript",
+    percentage: 80,
+  },
+  {
+    skill: "TypeScript",
+    percentage: 70,
+  },
+  {
+    skill: "Node.js",
+    percentage: 70,
+  },
+  {
+    skill: "Python",
+    percentage: 80,
+  },
+  {
+    skill: "MongoDB",
+    percentage: 80,
+  },
+  {
+    skill: "React.js",
+    percentage: 60,
+  },
+  {
+    skill: "Bootstrap",
+    percentage: 60,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Python backend",
+    description:
+      "12345",
+    from: 2021,
+    to: 2022,
+  },
+  
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portafolio1.jpeg",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
